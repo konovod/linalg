04c95de92bdaca91e1def703d11123f66e6ff41a
Konovod <konovod@gmail.com>
2017-06-24 20:01:41 +0000

    generalized eig problem (untested)

--- src/linalg/eig.cr
+++ src/linalg/eig.cr
@@ -3,13 +3,13 @@ require "./libLAPACKE"
 
 module Linalg
   module Matrix(T)
-    def eigvals(*, overwrite_a = false, b : Matrix(T)? = nil)
-      vals, aleft, aright = eigs(overwrite_a: overwrite_a, need_left: false, need_right: false, b: b)
+    def eigvals(*, overwrite_a = false)
+      vals, aleft, aright = eigs(overwrite_a: overwrite_a, need_left: false, need_right: false)
       vals
     end
 
-    def eigs(*, left = false, overwrite_a = false, b : Matrix(T)? = nil)
-      vals, aleft, aright = eigs(overwrite_a: overwrite_a, need_left: left, need_right: !left, b: b)
+    def eigs(*, left = false, overwrite_a = false)
+      vals, aleft, aright = eigs(overwrite_a: overwrite_a, need_left: left, need_right: !left)
       v = left ? aleft : aright
       {vals, v.not_nil!}
     end
@@ -34,7 +34,7 @@ module Linalg
       {% end %}
     end
 
-    def eigs(*, need_left : Bool, need_right : Bool, overwrite_a = false, b : Matrix(T)? = nil)
+    def eigs(*, need_left : Bool, need_right : Bool, overwrite_a = false)
       raise ArgumentError.new("matrix must be square") unless square?
       # TODO -  hb, sb, st
       # gg, sy
@@ -80,6 +80,38 @@ module Linalg
           vals = Array(Complex).new(rows){|i| Complex.new(reals[i], imags[i])}
         end
         return {vals, eigvectorsl, eigvectorsr}
+      {% end %}
+    end
+
+    # generalized eigenvalues problem
+    def eigs(*, b : Matrix(T), need_left : Bool, need_right : Bool, overwrite_a = false, overwrite_b = false)
+      a = overwrite_a ? self : clone
+      eigvectorsl = need_left ? GeneralMatrix(T).new(rows, rows) : nil
+      eigvectorsr = need_right ? GeneralMatrix(T).new(rows, rows) : nil
+      {% if T == Complex %}
+        alpha = Array(T).new(rows, T.new(0,0))
+        beta = Array(T).new(rows, T.new(0,0))
+        lapack(gg, ev, need_left ? 'V'.ord : 'N'.ord, need_right ? 'V'.ord : 'N'.ord, rows, a, rows,
+                overwrite_b ? b : b.clone, b.columns,
+                vals.to_unsafe.as(LibLAPACKE::DoubleComplex*),
+                eigvectorsl.try &.to_unsafe, rows,
+                eigvectorsr.try &.to_unsafe, rows)
+        return {alpha, beta, eigvectorsl, eigvectorsr}
+      {% else %}
+        alpha_reals = Array(T).new(rows, T.new(0))
+        alpha_imags = Array(T).new(rows, T.new(0))
+        beta = Array(T).new(rows, T.new(0))
+        lapack(gg, ev, need_left ? 'V'.ord : 'N'.ord, need_right ? 'V'.ord : 'N'.ord, rows, a, rows,
+                overwrite_b ? b : b.clone, b.columns,
+                alpha_reals, alpha_imags, beta,
+                eigvectorsl.try &.to_unsafe, rows,
+                eigvectorsr.try &.to_unsafe, rows)
+        if alpha_imags.all? &.==(0)
+          alpha = reals
+        else
+          alpha = Array(Complex).new(rows){|i| Complex.new(alpha_reals[i], alpha_imags[i])}
+        end
+        return {alpha, beta, eigvectorsl, eigvectorsr}
       {% end %}
     end
   end
