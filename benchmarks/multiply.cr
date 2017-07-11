93a7f1794275ba5d754950125cd2c7ff12309a77
Konovod <konovod@gmail.com>
2017-07-11 10:22:38 +0000

    fixed benchmark

--- benchmarks/multiply.cr
+++ benchmarks/multiply.cr
@@ -23,6 +23,15 @@ module Linalg::Matrix(T)
     result.inc_mult(self, m)
     result.tap { |r| r.flags = self.flags.mult(m.flags) }
   end
+
+  def ge_mult2(m : Matrix(T))
+    if ncolumns != m.nrows
+      raise ArgumentError.new("matrix size should match ([#{nrows}x#{ncolumns}] * [#{m.nrows}x#{m.ncolumns}]")
+    end
+    result = Matrix(T).zeros(nrows, m.ncolumns)
+    result.inc_mult(self, m, beta: 0.0)
+    result.tap { |r| r.flags = self.flags.mult(m.flags) }
+  end
 end
 
 def test(n)
@@ -32,38 +41,42 @@ def test(n)
   Benchmark.ips do |bench|
     bench.report("naive") { a.naive_mult(b) }
     bench.report("ge_mult") { a.ge_mult(b) }
+    bench.report("ge_mult2") { a.ge_mult2(b) }
   end
 end
 
 # check methods consistency
 a = Mat.rand(10, 10)
 a1 = a[0..5, 0..5]
-raise "failure" unless (a1*a1.inv).almost_eq Mat.eye(a1.nrows)
+raise "* failure" unless (a1*a1.inv).almost_eq Mat.eye(a1.nrows)
+raise "naive failure" unless (a1.naive_mult(a1.inv)).almost_eq Mat.eye(a1.nrows)
+raise "ge_mult failure" unless (a1.ge_mult(a1.inv)).almost_eq Mat.eye(a1.nrows)
+raise "ge_mult2 failure" unless (a1.ge_mult2(a1.inv)).almost_eq Mat.eye(a1.nrows)
 
 test(10)
 test(50)
 test(100)
 test(200)
 test(500)
-test(1000)
+# test(1000)
 
 # *********N = 10*************
-#   naive  35.43k ( 28.22µs) (±11.74%)  9.76× slower
-# ge_mult 345.96k (  2.89µs) (±22.20%)       fastest
+#    naive  35.48k ( 28.19µs) (±13.41%)  9.32× slower
+#  ge_mult 330.71k (  3.02µs) (±18.24%)       fastest
+# ge_mult2 327.29k (  3.06µs) (±22.18%)  1.01× slower
 # *********N = 50*************
-#   naive  340.6  (  2.94ms) (± 4.72%) 23.33× slower
-# ge_mult   7.95k (125.85µs) (±15.33%)       fastest
+#    naive  341.5  (  2.93ms) (± 4.31%) 23.16× slower
+#  ge_mult   7.91k (126.43µs) (±14.15%)       fastest
+# ge_mult2    7.5k (133.31µs) (±14.16%)  1.05× slower
 # *********N = 100*************
-#   naive  43.27  ( 23.11ms) (± 5.38%) 50.06× slower
-# ge_mult   2.17k (461.63µs) (± 2.64%)       fastest
+#    naive  43.29  (  23.1ms) (± 5.40%) 50.21× slower
+#  ge_mult   2.17k (460.11µs) (± 2.90%)       fastest
+# ge_mult2   2.15k ( 465.6µs) (± 2.62%)  1.01× slower
 # *********N = 200*************
-#   naive    5.2  (192.24ms) (± 5.82%) 60.85× slower
-# ge_mult 316.53  (  3.16ms) (± 2.31%)       fastest
+#    naive   5.18  (193.06ms) (± 4.35%) 59.46× slower
+#  ge_mult 307.52  (  3.25ms) (± 7.27%)  1.00× slower
+# ge_mult2 307.96  (  3.25ms) (± 2.17%)       fastest
 # *********N = 500*************
-#   naive   0.22  (  4.54s ) (± 0.60%) 100.15× slower
-# ge_mult  22.06  ( 45.34ms) (± 2.66%)        fastest
-# *********N = 1000*************
-# GC Warning: Repeated allocation of very large block (appr. size 8003584):
-# 	May lead to memory leak and poor performance
-#   naive   0.03  ( 33.12s ) (± 0.00%) 96.33× slower
-# ge_mult   2.91  (343.82ms) (± 1.61%)       fastest
+#    naive   0.22  (  4.58s ) (± 0.59%) 103.27× slower
+#  ge_mult  22.53  ( 44.39ms) (± 3.33%)        fastest
+# ge_mult2  22.46  ( 44.52ms) (± 3.58%)   1.00× slower
