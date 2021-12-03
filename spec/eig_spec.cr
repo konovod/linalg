2364b60398aac7da7e83ea0964636e1e50cd3555
Andrey Konovod <konovod@gmail.com>
2021-12-03 18:36:03 +0300

    let's try to identify problems on Mac

--- spec/eig_spec.cr
+++ spec/eig_spec.cr
@@ -23,8 +23,14 @@ describe LA do
     vals[1].should be_close 1 - 2.i, 1e-3
   end
 
-  it "high-level: calculate nonsymmetric eigenvectors" do
+  it "high-level: calculate nonsymmetric eigenvectors for single precision" do
     a = GMat32[[-2, 4, 1], [2, -4, 1], [1, 1, 1]]
+    vals, vectors = a.eigs
+    raise "" if vals.is_a? Array(Complex)
+    vals.each { |e| (a*vectors - vectors*e).det.should be_close 0, 1e-4 }
+  end
+  it "high-level: calculate nonsymmetric eigenvectors" do
+    a = GMat[[-2, 4, 1], [2, -4, 1], [1, 1, 1]]
     vals, vectors = a.eigs
     raise "" if vals.is_a? Array(Complex)
     vals.each { |e| (a*vectors - vectors*e).det.should be_close 0, 1e-4 }
