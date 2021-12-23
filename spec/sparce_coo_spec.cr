f63835abaadf85514eb4c3e6ef748cc3ff96c77b
Andrey Konovod <konovod@gmail.com>
2021-12-23 11:00:27 +0300

    construction of COO matrices

--- spec/sparce_coo_spec.cr
+++ spec/sparce_coo_spec.cr
@@ -3,7 +3,44 @@ require "./spec_helper"
 include LA
 include LA::Sparse
 describe COOMatrix do
-  m = GMat.eye(5)
-  ms = COOMatrix(Float64).new(m)
-  pp! m, ms
+  it "can be created from dense matrix" do
+    m = GMat.eye(5)
+    m[3, 1] = 2.0
+    ms = COOMatrix(Float64).new(m)
+    ms.should eq m
+  end
+
+  it "can be constructed from arrays" do
+    ms = COOMatrix(Float64).new(3, 4, [0, 1, 2], [1, 1, 2], [5.0, 10.0, 15.0])
+    ms.should eq GMat[
+      [0, 5, 0, 0],
+      [0, 10, 0, 0],
+      [0, 0, 15, 0],
+    ]
+    expect_raises(ArgumentError) { COOMatrix(Float64).new(3, 3, [0, 1, 3], [1, 1, 2], [5.0, 10.0, 15.0]) }
+    expect_raises(ArgumentError) { COOMatrix(Float64).new(3, 3, [0, 1, 2], [1, 1], [5.0, 10.0, 15.0]) }
+  end
+
+  it "can be created from another COO matrix" do
+    m = GMat.eye(5)
+    ms1 = COOMatrix(Float32).new(m)
+    ms1[3, 1] = 1.0
+    ms2 = COOMatrix(Float32).new(ms1)
+    ms2.should eq ms1
+    ms1[3, 1] = 2.0
+    ms2.should_not eq ms1
+
+    ms3 = COOMatrix(Float64).new(ms1)
+    ms3.should eq ms1
+  end
+
+  it "can be created empty" do
+    m = COOMatrix(Float64).new(2, 5)
+    m[0, 0] = 1.0
+    m[1, 2] = 5.0
+    m.should eq GMat[
+      [1, 0, 0, 0, 0],
+      [0, 0, 5, 0, 0],
+    ]
+  end
 end
--- src/matrix/sparse_matrix.cr
+++ src/matrix/sparse_matrix.cr
@@ -7,6 +7,8 @@ module LA
       getter nrows : Int32 = 0
       getter ncolumns : Int32 = 0
       property flags : MatrixFlags = MatrixFlags::None
+
+      abstract def nonzeros : Int32
     end
 
     class COOMatrix(T) < Matrix(T)
@@ -15,15 +17,19 @@ module LA
       protected getter raw_values : Array(T)
       protected getter dictionary = {} of Tuple(Int32, Int32) => Int32
 
-      def initialize(@nrows, @ncolumns)
-        @raw_rows = Array(Int32).new
-        @raw_columns = Array(Int32).new
-        @raw_values = Array(T).new
+      def initialize(@nrows, @ncolumns, capacity = 0)
+        @raw_rows = Array(Int32).new(capacity)
+        @raw_columns = Array(Int32).new(capacity)
+        @raw_values = Array(T).new(capacity)
       end
 
-      def initialize(@nrows, @ncolumns, rows : Array(Int32), columns : Array(Int32), values : Array(T), @flags = MatrixFlags::None, *, dont_clone : Bool = false, dictionary = nil)
-        raise "Can't construct COO Matrix from arrays of different size: rows.size=#{rows.size} != columns.size=#{columns.size}" if rows.size != columns.size
-        raise "Can't construct COO Matrix from arrays of different size: rows.size=#{rows.size} != values.size=#{values.size}" if rows.size != values.size
+      def initialize(@nrows, @ncolumns, rows : Array(Int32), columns : Array(Int32), values : Array(T), @flags = MatrixFlags::None, *, dont_clone : Bool = false, dont_check : Bool = false, dictionary = nil)
+        if rows.size != columns.size
+          raise ArgumentError.new("Can't construct COO Matrix from arrays of different size: rows.size=#{rows.size} != columns.size=#{columns.size}")
+        end
+        if rows.size != values.size
+          raise ArgumentError.new("Can't construct COO Matrix from arrays of different size: rows.size=#{rows.size} != values.size=#{values.size}")
+        end
         if dont_clone
           @raw_rows = rows
           @raw_columns = columns
@@ -37,11 +43,22 @@ module LA
           @dictionary = dictionary.dup
         else
           @dictionary = {} of Tuple(Int32, Int32) => Int32
-          n = @raw_rows.size
-          n.times do |i|
-            @dictionary[{@raw_rows[i], @raw_columns[i]}] = i
+          nonzeros.times do |i|
+            row = @raw_rows[i]
+            unless (0...@nrows).includes?(row)
+              raise ArgumentError.new("Can't construct COO Matrix from arrays: rows[#{i}]=#{@raw_rows[i]} is outside (#{0}..#{@nrows})")
+            end
+            column = @raw_columns[i]
+            unless (0...@ncolumns).includes?(column)
+              raise ArgumentError.new("Can't construct COO Matrix from arrays: columns[#{i}]=#{@raw_columns[i]} is outside (#{0}..#{@ncolumns})")
+            end
+            @dictionary[{row, column}] = i
           end
         end
+      end
+
+      def nonzeros : Int32
+        @raw_rows.size
       end
 
       def self.new(matrix : COOMatrix(T))
@@ -53,9 +70,13 @@ module LA
       end
 
       def self.new(matrix : LA::Matrix)
-        result = new(matrix.nrows, matrix.ncolumns)
+        if matrix.is_a? Sparse::Matrix
+          result = new(matrix.nrows, matrix.ncolumns, matrix.nonzeros)
+        else
+          result = new(matrix.nrows, matrix.ncolumns)
+        end
         matrix.each_with_index(all: false) do |v, i, j|
-          result.unsafe_set(i, j, v)
+          result.push_element(i, j, v)
         end
         result
       end
@@ -68,16 +89,29 @@ module LA
         end
       end
 
+      protected def push_element(i, j, v)
+        @raw_rows << i
+        @raw_columns << j
+        @raw_values << T.new(v)
+        @dictionary[{i, j}] = @raw_rows.size - 1
+      end
+
+      def add_element(i, j, v)
+        clear_flags # TODO - not always?
+        if index = @dictionary[{i, j}]?
+          @raw_values[index] += T.new(value)
+        else
+          push_element(i, j, v)
+        end
+      end
+
       def unsafe_set(i, j, value)
         clear_flags # TODO - not always?
         if index = @dictionary[{i, j}]?
           @raw_values[index] = T.new(value)
         else
           # TODO - delete elements?
-          @raw_rows << i
-          @raw_columns << i
-          @raw_values << T.new(value)
-          @dictionary[{i, j}] = @raw_rows.size - 1
+          push_element(i, j, value)
         end
       end
 
