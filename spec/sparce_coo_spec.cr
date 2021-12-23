3ba27bed5dff82502d00971ca4868283b9db808e
Andrey Konovod <konovod@gmail.com>
2021-12-23 17:41:27 +0300

    specs and fixes for COO features

--- spec/sparce_coo_spec.cr
+++ spec/sparce_coo_spec.cr
@@ -36,11 +36,116 @@ describe COOMatrix do
 
   it "can be created empty" do
     m = COOMatrix(Float64).new(2, 5)
+    m.flags.diagonal?.should be_true
     m[0, 0] = 1.0
     m[1, 2] = 5.0
     m.should eq GMat[
       [1, 0, 0, 0, 0],
       [0, 0, 5, 0, 0],
     ]
+    m.flags.none?.should be_true
+  end
+
+  it "elements can be added and removed" do
+    m = COOMatrix(Float64).new(4, 2)
+    m.nonzeros.should eq 0
+    m[3, 1] = 1.0
+    m.nonzeros.should eq 1
+    m[3, 1] = 2.0
+    m.nonzeros.should eq 1
+    m.should eq GMat[
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 2],
+    ]
+    m[3, 1] = 0.0
+    m.nonzeros.should eq 0
+    m.should eq GMat.new(4, 2)
+  end
+
+  it "can be cleared" do
+    m = COOMatrix(Float64).new(GMat32.eye(5))
+    m.clear
+    m.nonzeros.should eq 0
+    m.should eq GMat32.new(5, 5)
+  end
+
+  it "can be transposed" do
+    m = COOMatrix(Float64).new(GMat32.eye(5))
+    m[3, 2] = 20.0
+    mt = m.transpose
+    mt[3, 2].should eq 0
+    mt[2, 3].should eq 20.0
+    mt.should_not eq m
+    m.transpose!
+    mt.should eq m
+  end
+
+  it "can be initialized with defined number of random values" do
+    rng1 = Random.new(1)
+    m1 = COOMatrix(Complex).rand(50, 50, nonzero_elements: 4, rng: rng1)
+    m1.nonzeros.should eq 4
+    rng2 = Random.new(1)
+    m2 = COOMatrix(Complex).rand(50, 50, nonzero_elements: 4, rng: rng2)
+    m3 = COOMatrix(Complex).rand(50, 50, nonzero_elements: 4)
+    m3.nonzeros.should eq 4
+    m1.should eq m2
+    m1.should_not eq m3
+    expect_raises(ArgumentError) { COOMatrix(Complex).rand(5, 5, nonzero_elements: 26) }
+  end
+
+  it "can be initialized with defined percentage of random values" do
+    rng1 = Random.new(1)
+    m1 = COOMatrix(Complex).rand(50, 50, fill_factor: 0.1, rng: rng1)
+    m1.nonzeros.should be_close (50*50*0.1), 50*50*0.05
+    rng2 = Random.new(1)
+    m2 = COOMatrix(Complex).rand(50, 50, fill_factor: 0.1, rng: rng2)
+    m3 = COOMatrix(Complex).rand(50, 50, fill_factor: 0.1)
+    m3.nonzeros.should be_close (50*50*0.1), 50*50*0.05
+    m1.should eq m2
+    m1.should_not eq m3
+  end
+
+  it "support arithmetic operations" do
+    ma = GMat[
+      [1, 2, 0],
+      [0, 0, 5],
+      [0, 1, 0],
+      [-2, -1, -3],
+    ]
+    mb = GMat[
+      [0, -2, 0],
+      [0, 1, -5],
+      [0, -1, 1],
+      [10, 0, 0],
+    ]
+    mas = COOMatrix(Float64).new(ma)
+    mbs = COOMatrix(Float64).new(mb)
+
+    (mas + mbs).should eq (ma + mb)
+    (3*mas - 6*mbs).should eq (3*ma - 6*mb)
+    mas.add!(-5, mbs)
+    mas.should_not eq ma
+    ma.add!(-5, mb)
+    mas.should eq ma
+  end
+
+  it "support #tril and #triu" do
+    m = COOMatrix(Float64).new(10, 10)
+    m[0, 0] = 10
+    m[1, 1] = 15
+    m[5, 2] = 11
+    m[3, 6] = 12
+
+    m.tril(2).should eq m.to_dense.tril(2)
+    m.tril.should eq m.to_dense.tril
+    m.triu(2).should eq m.to_dense.triu(2)
+    m.triu.should eq m.to_dense.triu
+
+    ma = m.triu(1)
+    m.should_not eq ma
+    m.triu!(1)
+    m.should eq ma
   end
 end
